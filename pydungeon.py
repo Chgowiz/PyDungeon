00031ff50b25e6e453c9a58fa66820eb847ea526
Michael S <chgowiz@live.com>
2020-06-02 15:49:49 -0500

    Much needed cleanup - created a game_state data struct to make code less messy. Coded attack routine.

--- pydungeon.py
+++ pydungeon.py
@@ -1,5 +1,8 @@
 # Recreation of DUNGEON, a game written by Brian Sawyer for Cursor Magazine,
 # issue #15. Published in Dec 1979. 
+# Michael Shorten, June 2020, Public Domain, I have no rights to original
+# source. I am doing this for educational purposes to demonstrate computer
+# history and what it might look like in modern programming.
 
 from random import random, randint, choice
 import sys
@@ -11,7 +14,7 @@ WIDTH = 40
 HEIGHT = 25
 RM_GEN_RETRIES = 50
 DESIRED_RMS = 8
-MONSTERS = ["R","G","D","S","N","W"]
+MONSTERS = ["X","G","D","S","N","W"]
 FLOOR = "."
 DOOR = "+"
 GOLD = "g"
@@ -20,6 +23,49 @@ BORDER = "*"
 PLAYER = "@"
 VALIDMOVES = "qs123456789"
 MOVEMAP = [41,80,81,82,40,41,42,0,1,2]
+PRINT_PAUSE = 1
+
+class GameState():
+
+    def __init__(self):
+        # RS/CS are row size, col size for display.
+        # The other variables are used for screen painting purposes.
+        # TS and AX are key variables in the game, they were used to mark the points 
+        # where the program could generate an in-memory map (TS) and then AX was the
+        # point where PET screen memory began. POKEing to locations at AX and beyond
+        # would write to the screen!
+        # We don't really need TS or AX in this program, so I've done away with them.
+        self.RS=23; self.CS=40
+        self.SZ=self.RS*self.CS
+        self.BL=(25-self.RS)*40
+        self.RS-=1; self.CS-=1
+
+        self.dungeon_map = []
+        self.player_map = []
+
+        self.hidden_gold = NUM_HIDDEN_GOLD
+        self.gold_stash = 0
+        self.found_gold = False
+
+        self.whats_here = FLOOR
+        self.see_more = False
+        self.shift_mode = 0
+
+        self.player_loc = 0
+        self.player_HP = 50
+        self.player_gold = 0
+        self.player_experience = 0
+        self.monsters_killed = 0
+        self.next_level = 0
+
+        self.active_monster = ""
+        self.monster_name = ""
+        self.monster_level = 0
+        self.monster_hp = 0
+        self.monster_loc = 0
+        self.monster_S = 0
+        self.prev_monster = ""
+
 
 def cls():
     if name == "nt":
@@ -27,14 +73,16 @@ def cls():
     else:
         _ = system("clear")
 
+
 def display_welcome():
     cls()
     print("          PyDungeon\n    A recreation of CURSOR #15 DUNGEON")
     print("Original (C)1979 by Brian Sawyer.\nPyDungeon is public domain.")
     print("-" * 40)
-    print("Search for GOLD in the ancient ruins\n")
+    print_pause("Search for GOLD in the ancient ruins\n")
     print("Press RETURN to begin")
     input()
+
 
 def init_map():
     # Create new map/screen data structure
@@ -167,13 +215,16 @@ def gen_dungeon(SZ, RS, CS):
 
     return mem
 
-def what_is_seen(dungeon_map, players_map, L, SM, MG, K1):
+
+def what_is_seen(game_state):
 
     ## BUG ## - the view "wraps" if we're right next to the border. The border needs
     # to stop the view
+    
+    gold_near = False
 
-    if SM==1:
-        K=80; J=-2; R=3; SM=0
+    if game_state.see_more:
+        K=80; J=-2; R=3; game_state.see_more = False
     else:
         K=40; J=-1; R=2
 
@@ -181,31 +232,132 @@ def what_is_seen(dungeon_map, players_map, L, SM, MG, K1):
         for N1 in range(J,R):       # row position - above, center, below
             if N==0 and N1==0:      # if we're looking at our curr position...
                 continue            # ... then ignore. We know what's here!
-            Y=L+N+N1
-            V=PEEK(dungeon_map, Y)
-            POKE(players_map, Y, V)
+            Y=game_state.player_loc+N+N1
+            V=PEEK(game_state.dungeon_map, Y)
+            POKE(game_state.player_map, Y, V)
+
             if V==DOOR or V==FLOOR or V==BORDER:
                 continue
             if V==GOLD:
-                K1=1+K1+int((MG+1)*(random()))  # We found gold! How much?
-                                                # See notes on source ln 680
+                # We found gold! How much?
+                # See notes on source ln 680
+                game_state.gold_stash += 1+int(
+                    (game_state.player_gold+1)*(random()))  
+                gold_near = True
+                # If we've not been near this gold, announce it.
+                if not game_state.found_gold:
+                    print_pause("Gold is near!")
+                
                 continue
             if V in MONSTERS:
+                if V=="X":
+                    game_state.monster_name="Spider"
+                    game_state.monster_level=3
+                elif V=="G":
+                    game_state.monster_name="Grue"
+                    game_state.monster_level=7
+                elif V=="D":
+                    game_state.monster_name="Dragon"
+                    game_state.monster_level=1
+                elif V=="S":
+                    game_state.monster_name="Snake"
+                    game_state.monster_level=2
+                elif V=="N":
+                    game_state.monster_name="Nuibus"
+                    game_state.monster_level=9
+                elif V=="W":
+                    game_state.monster_name="Wyvern"
+                    game_state.monster_level=5
+
+                game_state.active_monster = V
+                
+                ## TODO - uncomment this when we want to have the active
+                # monster move.
+                # POKE(game_state.dungeon_map, Y, FLOOR)
+
+                game_state.monster_S = 0
+
+                # Monster HP based on our HP, experience and random
+                # We save the level for later if we defeat the monster, to 
+                # get XP from!
+                # It will change each time we see this monster. Heh.
+                game_state.monster_hp = game_state.monster_level = \
+                    int(random()*game_state.player_HP + 
+                     (game_state.player_experience/game_state.monster_level) + 
+                     game_state.player_HP/4)
+
+                # If we've already revealed a monster, put it back on the
+                # dungeon map.
+                if game_state.monster_loc > 0:
+                    POKE(game_state.dungeon_map, game_state.monster_loc, 
+                         game_state.prev_monster)
+                game_state.prev_monster = game_state.active_monster
+                game_state.monster_loc = Y
+                print_pause("A {} with {} points is near!".format(
+                    game_state.monster_name, game_state.monster_hp))
+
                 continue
-    #710 V1=V+128:S=0:POKEY+TS,160
-    #720 IFV=86THENE$="SPIDER":I=3
-    #730 IFV=87THENE$="GRUE":I=7
-    #740 IFV=88THENE$="DRAGON":I=1
-    #750 IFV=89THENE$="SNAKE":I=2
-    #760 IFV=90THENE$="NUIBUS":I=9
-    #770 IFV=91THENE$="WYVERN":I=5
-    #780 I=INT(RND(1)*HP+(PX/I)+HP/4)
-    #790 IFE>0THENPOKETS+E,QQ
-    #800 QQ=V+128:E=Y
-    #810 GOSUB1410:PRINT"A "E$;" WITH";I;"POINTS IS NEAR.":GOSUB1430:CC=I
-    #820 NEXTN1:NEXTN:FG=GN:RETURN
     
-    return K1, SM
+    game_state.found_gold = gold_near
+
+def attack(game_state):
+    print_pause("## AN ATTACK! ##")
+    player_power = game_state.player_HP+game_state.player_experience
+    monster_attack=random()*game_state.monster_hp/2+game_state.monster_hp/4
+    player_attack=random()*player_power/2+player_power/4
+    game_state.monster_hp=int(game_state.monster_hp-player_attack)
+    game_state.player_HP=int(game_state.player_HP-monster_attack)
+
+    # Is the PC dead? If not, continue with attack results
+    if game_state.player_HP > 0:
+        # If monster is twice as strong as player, it will make
+        # an offer that the player can't refuse...
+        if (game_state.monster_hp/game_state.player_HP+1)>2:
+            print_pause("The {} will leave, IF you will give it half your gold.".format(
+                game_state.monster_name))
+            responded = False
+            answer = ""
+            while not responded:
+                answer = input("Will you consent to this (Y or N)? ").lower().strip()
+                if answer.startswith("y") or answer.startswith("n"):
+                    responded = True
+            if answer == "y":
+                # Take the gold, the monster disappears!
+                game_state.player_gold -= int(game_state.player_gold/2)
+                remove_monster(game_state)
+                return
+        elif game_state.monster_hp > 0:
+            print_pause("The {} has {} hit points".format(game_state.monster_name,
+                                                    game_state.monster_hp))
+            return
+        else:
+            # The monster is dead
+            game_state.player_experience += game_state.monster_level
+            game_state.monsters_killed += 1
+            print_pause("The {} is dead!".format(game_state.monster_name))
+            remove_monster(game_state)
+            if game_state.player_experience >= game_state.next_level * 2:
+                game_state.next_level = game_state.player_experience
+                game_state.player_HP *= 3
+                print_pause("Your hit pts. have been raised")
+                return
+    else:
+        # Death routine will be handled from check in main loop.
+        return
+    
+
+def remove_monster(game_state):
+    game_state.whats_here = FLOOR
+    game_state.monster_loc = 0
+    game_state.active_monster = ""
+    game_state.monster_S = 0
+    POKE(game_state.player_map, game_state.player_loc, PLAYER)
+
+
+def print_pause(msg):
+    print(msg)
+    sleep(PRINT_PAUSE)
+
 
 def display_dungeon_map(map, final=False):
     cls()
@@ -214,8 +366,12 @@ def display_dungeon_map(map, final=False):
         if final:
             sleep(.5)
 
-def get_player_input(HP, EX, MG):
-    print("HIT PTS. {}  EXP. {}  GOLD {}".format(int(HP+.5), EX, MG))
+
+def get_player_input(game_state):
+    print("HIT PTS. {}  EXP. {}  GOLD {}".format(
+        int(game_state.player_HP+.5), 
+        game_state.player_experience, 
+        game_state.player_gold))
     print("You may move. ", end="")
 
     move = input().lower().strip()
@@ -224,10 +380,14 @@ def get_player_input(HP, EX, MG):
     return move
 
 
-def end_game(MG, EX, Z, end_message):
-    print(end_message)
-    print("Gold: {} Exp: {} Killed {} Beasts".format(MG, EX, Z))
+def end_game(game_state, end_message):
+    print_pause(end_message)
+    print("Gold: {} Exp: {} Killed {} Beasts".format(
+        game_state.player_gold, 
+        game_state.player_experience, 
+        game_state.monsters_killed))
     input("Press return to see the dungeon map. ")
+
 
 # Display welcome
 display_welcome()
@@ -238,44 +398,29 @@ while True:
     cls()
     print("Setting up...")
 
-    # RS/CS are row size, col size for display.
-    # The other variables are used for screen painting purposes.
-    # TS and AX are key variables in the game, they were used to mark the points 
-    # where the program could generate an in-memory map (TS) and then AX was the
-    # point where PET screen memory began. POKEing to locations at AX and beyond
-    # would write to the screen!
-    # We don't really need TS or AX in this program, so I've done away with them.
-    RS=23; CS=40; SZ=RS*CS; BL=(25-RS)*40 ; RS=RS-1; CS=CS-1
+    # Look at annotated source and line 210 to see several of the variables needed
+    # to be tracked through this game. Everything was global. To manage that mess,
+    # I created a class struct to hold the game state.
+    game = GameState()
 
     # Dungeon Map is the generated map
-    dungeon_map = gen_dungeon(SZ, RS, CS)
+    game.dungeon_map = gen_dungeon(game.SZ, game.RS, game.CS)
 
     # Player Map is what is displayed to the player as they navigate the dungeon
-    player_map = init_map()
-
-    # 210 HP=50:MG=0:EX=0:PX=0:HG=0:Z=0:FG=0:K1=0:E=0:S=0:W=160:ET=160
-    # HP is player hit points; MG is gold recovered; EX is experience earned
-    # HG is hidden gold; Z is number of monsters killed; SX is "shift mode" to move
-    # thru walls [not sure how I'm going to implement that yet...]; W is what was in
-    # the space that the player just moved thru; K1 is how much gold has been uncovered; 
-    # SM is "see mode" - look further. Not well or completely implemented by Brian.
-
-    HP=50; MG=0; EX=0; HG=NUM_HIDDEN_GOLD; Z=0; SX=0; W=FLOOR; K1=0; 
-    SM=0
+    game.player_map = init_map()
 
     good_location = False
     while not good_location:
-        L = int(random()*SZ)
-        good_location = (PEEK(dungeon_map, L) == FLOOR)
+        game.player_loc = int(random()*game.SZ)
+        good_location = (PEEK(game.dungeon_map, game.player_loc) == FLOOR)
     
-    # 260 TM=0:GOSUB1410:L=L+AX-TS:W=PEEK(L):GOSUB600:POKEL,209:W=160
-    W = PEEK(dungeon_map, L)
+    game.whats_here = PEEK(game.dungeon_map, game.player_loc)
 
     # Determine/display what is visible
-    K1, SM = what_is_seen(dungeon_map, player_map, L, SM, MG, K1)
+    what_is_seen(game)
 
-    POKE(player_map, L, PLAYER)
-    W = FLOOR
+    POKE(game.player_map, game.player_loc, PLAYER)
+    game.whats_here = FLOOR
 
     # Input/Move loop
     # This is not set up like DUNGEON!
@@ -288,22 +433,24 @@ while True:
         map_move = False
         # Eligible moves are 1-9 for directions
         # except 5, which is a "wait" (and heal!)
-        # S which is --notsure-- and q which is quit.
+        # S is 'see more' and q is quit.
         # so if 5 or S, update HP, continue to get input.
         # if q, then break out of this while and stop playing. 
         # if invalid input (""), continue to get input till valid.
-        # Otherwise, continue on to calculate the results of the move
+        # Otherwise, continue on to calculate the results of the move!
         while not map_move:
-            display_dungeon_map(player_map)
-            move = get_player_input(HP, EX, MG)
+            display_dungeon_map(game.player_map)
+            move = get_player_input(game)
 
             if move == "":
                 pass
             elif move == "5":     # Rest/recover HP
-                HP += 1+sqrt(EX/HP)
+                game.player_HP += 1+sqrt(game.player_experience/game.player_HP)
                 map_move = True
+                move = 5
             elif move == "s":     # See more mode
-                SM = 1; HP -= 2
+                game.see_more = True
+                game.player_HP -= 2
                 map_move = True   # Force a map move/screen refresh
                 move = 5          # but make it a "stay put" move.
             elif move == "q":     # Quit game
@@ -315,8 +462,8 @@ while True:
 
         # You lose HP as you move, doubly so if you are in shift
         # mode to move through walls. If we drop below 0XP, end of game.
-        HP=HP-.15-2*SX 
-        if HP < 0:
+        game.player_HP -= .15 - 2 * game.shift_mode
+        if game.player_HP <= 0:
             playing = False
             break
 
@@ -330,34 +477,50 @@ while True:
         # grid spaces (assume 40c x 25r screen) to move the player.
         if map_move: 
             Q = MOVEMAP[move]-41
-            if (PEEK(dungeon_map,L+Q)==" " and SX!=1) or \
-                (PEEK(dungeon_map,L+Q)==BORDER):
+            if (PEEK(game.dungeon_map,game.player_loc+Q)==" " \
+                and game.shift_mode!=1) or \
+                (PEEK(game.dungeon_map,game.player_loc+Q)==BORDER):
                 continue
             else:
-                POKE(player_map,L,W)
-                L=L+Q
-                W = PEEK(dungeon_map,L)
-                POKE(player_map,L,PLAYER)
+                POKE(game.player_map,game.player_loc,game.whats_here)
+                game.player_loc+=Q
+                game.whats_here = PEEK(game.dungeon_map,game.player_loc)
+                POKE(game.player_map,game.player_loc,PLAYER)
 
-                K1, SM = what_is_seen(dungeon_map, player_map, L, SM, MG, K1)
+                # What do we see now as a result of the move?
+                #CC,E,S,V1,ESTR = what_is_seen()
+                what_is_seen(game)
 
-                if W == GOLD:
-                    MG=MG+K1
-                    print("You found {} gold pieces!".format(K1))
-                    POKE(dungeon_map, L, FLOOR)
-                    W = FLOOR
-                    HG -= 1
-                    if HG == 0:
+                # If we moved onto gold...
+                if game.whats_here == GOLD:
+                    game.player_gold+=game.gold_stash
+                    print_pause("You found {} gold pieces!".format(game.gold_stash))
+                    POKE(game.dungeon_map, game.player_loc, FLOOR)
+                    game.whats_here = FLOOR
+                    game.hidden_gold -= 1
+                    if game.hidden_gold == 0:
+                        playing = False
+                        break
+                
+                # If we move onto a monster, attack!
+                if game.whats_here in MONSTERS:
+                    attack(game)
+
+                    if game.player_HP <= 0:
                         playing = False
                         break
 
-    if HP <= 0:
-        end_game(MG,EX,Z,"You're dead!")
-    if HG == 0:
-        end_game(MG,EX,Z,"You found all the gold! You won!")
+                # If there's an active monster, it moves, and
+                # can attack, same "round"
+
+
+    if game.player_HP <= 0:
+        end_game(game,"You're dead!")
+    if game.hidden_gold == 0:
+        end_game(game,"You found all the gold! You won!")
 
     # Display the dungeon map
-    display_dungeon_map(dungeon_map, True)
+    display_dungeon_map(game.dungeon_map, True)
 
     print("Want to play again? ", end="")
     if not input().lower().startswith("y"):
